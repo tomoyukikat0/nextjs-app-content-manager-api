9a0712a6605b099e909aa1be4ba6b748a90859a8
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-25 14:54:24 +0900

    create server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Server is running!");
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+const fs = require("fs");
+const path = require("path");
+const pathToFile = path.resolve("./data.json");
+
+const getResources = () => JSON.parse(fs.readFileSync(pathToFile))
+
+console.log(pathToFile);
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+app.get("/api/resources", (req, res) => {
+  const resources = getResources();
+  res.send(resources)
+})
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port:" + PORT);
+})
