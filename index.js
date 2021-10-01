e07ec216535fb42cc625d0971f09dd591479679f
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-10-01 16:33:28 +0900

    update api server

--- index.js
+++ index.js
@@ -21,6 +21,44 @@ app.get("/api/resources/:id", (req, res) => {
   res.send(resource);
 })
 
+app.patch("/api/resources/:id", (req, res) => {
+  const resources = getResources();
+  const { id } = req.params;
+  const index = resources.findIndex(resource => resource.id === id);
+  const activeResource = resources.find(resource => resource.status === "active")
+
+  if (resources[index].status === "complete") {
+    return res.status(422).send("Cannot update because resource has been completed!");
+  }
+
+  resources[index] = req.body;
+
+  // active resource related functionality
+  if (req.body.status === "active") {
+    if (activeResource) {
+      return res.status(422).send("There is active resource already!");
+    }
+
+    resources[index].status = "active";
+    resources[index].activationTime = new Date();
+  }
+  // active resource related functionality
+
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send("Cannot store data in the file!");
+    }
+
+    return res.send("Data has been updated!");
+  })
+})
+
+app.get("/api/activeresource", (req, res) => {
+  const resources = getResources();
+  const activeResource = resources.find(resource => resource.status === "active");
+  res.send(activeResource);
+})
+
 app.get("/api/resources", (req, res) => {
   const resources = getResources();
   res.send(resources);
